0d8ab931204100d4c559526096502969e85a4fe7
David Dias <david6dias@gmail.com>
2022-12-03 11:20:19 +0000

    Add tests file

---
+++ tests/room/tests.cpp
@@ -0,0 +1,17 @@
+/** \file tests.cpp
+ * Room class tests.
+ */
+
+/**
+ * Lets pretend this is a test.
+ * 
+ * @return TODO.
+ */
+double testCalculateArea(){   
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    return testCalculateArea();
+}
